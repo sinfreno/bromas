caabf716c67b8c7b8de8f0546fd29d95ac799077
Carlos Perez <cperezwuait@gmail.com>
2021-03-08 19:08:06 +0100

    Bromas

---
+++ bromas.js
@@ -0,0 +1,20 @@
+// Funcion al clicar sobre el boton
+
+
+// Método .then
+// function showJoke(){
+
+// fetch('https://icanhazdadjoke.com/slack')
+//   .then(res => res.json())
+//   .then(res => console.log(res.attachments[0].text));
+
+// }
+
+  
+// Método async/await
+async function showJoke() {
+    const result = await fetch('https://icanhazdadjoke.com/slack');
+    const res = await result.json();
+    console.log(res.attachments[0].text);
+};
+
